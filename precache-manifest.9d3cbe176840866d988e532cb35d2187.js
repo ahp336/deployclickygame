cfc4b8d6a27e8876754317b04be2a168d22ed0d8
ahp336 <aakashparikh8@gmail>
2019-11-07 16:53:39 -0500

    Updates

---
+++ precache-manifest.9d3cbe176840866d988e532cb35d2187.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c68270fa6fee928196e389cb401edd",
+    "url": "/deployclickygame/index.html"
+  },
+  {
+    "revision": "2f6214951c8fbd41fb42",
+    "url": "/deployclickygame/static/css/main.35c522bd.chunk.css"
+  },
+  {
+    "revision": "8dc487a124af900f84eb",
+    "url": "/deployclickygame/static/js/2.7d00664a.chunk.js"
+  },
+  {
+    "revision": "2f6214951c8fbd41fb42",
+    "url": "/deployclickygame/static/js/main.df7338fe.chunk.js"
+  },
+  {
+    "revision": "4f4b52cb0bda8bec8f09",
+    "url": "/deployclickygame/static/js/runtime-main.300bf50d.js"
+  }
+]);
